a60d823980a35b8593ff9bf0a969503df4a6945b
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2022-08-20 16:05:58 +0530

    Customisation Update

---
+++ pages/index.tsx
@@ -0,0 +1,61 @@
+import Head from "next/head"
+import Link from "next/link"
+import Date from "../components/date"
+import Layout, { siteTitle} from "../components/layout"
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import { GetStaticProps } from 'next'
+      
+export default function Home({ allPostsData }:{
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+  }) {
+    return (
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>
+            Hello, readers, We're a team of a Social Media Agency. You can contact
+            us on{" "}
+            <Link href="https://instagram.com/technicalqueries">
+              <a className={utilStyles.instagram}>Instagram</a>
+            </Link>
+          </p>
+          <p>
+            (This is a sample website - you’ll be building a site like this on{" "}
+            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Layout>
+    );
+  }
+
+export const getStaticProps:GetStaticProps = async() => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
