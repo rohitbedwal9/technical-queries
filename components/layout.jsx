13e9fbe57cd5ae45d6657390167716cd4380462b
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2022-08-25 12:02:12 +0530

    Update layout.jsx

--- components/layout.jsx
+++ components/layout.jsx
@@ -30,7 +30,7 @@ export default function Layout({children,home}){
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/Profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
