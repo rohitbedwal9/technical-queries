50155e63f1bc243cca508f18796b74f5bf4b1702
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2022-08-25 13:45:35 +0530

    logo error fix

--- components/layout.jsx
+++ components/layout.jsx
@@ -30,7 +30,7 @@ export default function Layout({children,home}){
           <>
             <Image
               priority
-              src="/images/Profile.jpg"
+              src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
